c7861579ce598f1967139b50155d4ad39c2e1aa5
Luca Favini <luca.favini.02@gmail.com>
2023-01-12 12:11:02 +0100

    Playfy: add Brano constructur, Brano.repOk

--- Playfy/src/Brano.java
+++ Playfy/src/Brano.java
@@ -1,5 +1,28 @@
 public record Brano(Album album, int posizione) implements Ascoltabile {
 
+  /**
+   * Crea un nuovo Brano estratto da un album in base alla sua posizione
+   * 
+   * @param album da cui estrarre il brano
+   * @param posizione del brano da estrarre (rispetto all'album)
+   * @throws NullPointerException se album è null
+   * @throws IndexOutOfBoundsException se la posizione è invalida rispetto all'album
+   */
+  public Brano(Album album, int posizione) {
+    if (album == null) {
+      throw new NullPointerException("L'album del brano non può essere null");
+    }
+
+    try {
+      album.getBrano(posizione);
+    } catch (IndexOutOfBoundsException e) {
+      throw new IndexOutOfBoundsException("La posizione del brano deve essere valida all'interno dell'album");
+    }
+
+    this.album = album;
+    this.posizione = posizione;
+  }
+
   @Override
   public String nome() {
     return album.getNomeBrano(posizione);
@@ -8,6 +31,22 @@ public record Brano(Album album, int posizione) implements Ascoltabile {
   @Override
   public int durata() {
     return album.getDurataBrano(posizione);
+  }
+
+  /**
+   * RI:
+   *  album != null
+   *  posizione valida in album
+   */
+  private boolean repOk() {
+    if (album == null) return false;
+    try {
+      album.getBrano(posizione);
+    } catch (IndexOutOfBoundsException e) {
+      return false;
+    }
+
+    return true;
   }
   
 }
