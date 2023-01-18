3c8a5570fb7d4cc83b999f2988bd5208a391cd4d
Luca Favini <luca.favini.02@gmail.com>
2023-01-18 13:51:41 +0100

    ScaloNavale: add test class

--- ScaloNavale/src/Test.java
+++ ScaloNavale/src/Test.java
@@ -1,19 +1,49 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        Scalo scalo = new Scalo(5);
-        Rimorchiatore rim = new RimorchiatoreMini("rim", scalo);
-        scalo.setRimorchiatore(rim);
-        //Scalo scalo = new Scalo(5, new RimorchiatoreSuper());
+        Scanner in = new Scanner(System.in);
+
+        int numMoli = in.nextInt();
+        Scalo scalo = new Scalo(numMoli);
+
+        for (int i = 0; in.hasNextLine(); i++) {
+            String line = in.nextLine();
+            if (line.isBlank()) {
+                i--;
+                continue;
+            }
+            String[] tokens = line.split(" ");
+            for (int j = 0; j < tokens.length; j+=2 ) {
+                scalo.attracca(new NaveCargo(tokens[j], Integer.parseInt(tokens[j+1])), i);
+            }
+        }
+        in.close();
         
-        scalo.attracca(new NaveCargo("a", 10), 1);
-        
-        scalo.attracca(new NaveCargo("b", 100), 1);
-    
-        scalo.attracca(new NaveCargo("c", 100), 1);
+        System.out.print(scalo);
 
-        scalo.sposta(3, 1, 2);
+        int qty = Integer.parseInt(args[0]);
+        int from = Integer.parseInt(args[1]);
+        int to = Integer.parseInt(args[2]);
 
-        System.out.println(scalo);
+        Rimorchiatore rim;
+        switch (args[3]) {
+        case "mini":
+            rim = new RimorchiatoreMini("rim", scalo);
+            break;    
+        case "super":
+            rim = new RimorchiatoreSuper("rimsuper", scalo);
+            break;
+        case "prudente":
+            rim = new RimorchiatorePrudente("rimprudente", scalo, Integer.parseInt(args[4]));
+            break;
+        default:
+            rim = new RimorchiatoreMini("temp", scalo);
+            break;
+        }
+        int steps = rim.sposta(qty, from, to);
 
+        System.out.println(steps);
+        System.out.print(scalo);
     }
 }
